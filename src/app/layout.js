95e7c6fa750989d0f46ee0f1fe84ff31550dc547
arpantiwari16 <128488031+arpantiwari16@users.noreply.github.com>
2024-03-13 11:04:43 +0530

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className='relative min-h-screen'>{children}
       <FooterSearch/></body>
     </html>
   );
