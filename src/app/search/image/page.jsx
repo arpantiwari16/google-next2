82e6bfd45edfdb93f36a522f60b0bbe1d7800950
arpantiwari16 <128488031+arpantiwari16@users.noreply.github.com>
2024-03-13 11:05:38 +0530

    Add files via upload

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,9 +1,27 @@
+import ImageSearch from '@/components/ImageSearch';
 import React from 'react'
 
-function ImageSearchPage() {
+async function WebImagePage({searchParams}) {
+  const startIndex = searchParams.start || '1';
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}
+    &cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}
+    &searchType=image&start=${startIndex}`
+
+  );
+  const data=await response.json();
+  const result=data.items;
+  // console.log(data);
   return (
-    <div>ImageSearchPage</div>
+    <div>
+      {
+        // console.log(data)
+        result && <ImageSearch results={data}/>
+        // <h1>hello</h1>
+      }
+    </div>
+
   )
 }
 
-export default ImageSearchPage
+export default WebImagePage
